077da8af707c4b824852dfd5292e09ceefaa29f3
Thomas Upfold <thomasupfold@gmail.com>
2018-07-03 00:11:45 +1000

    added in steering smoothing and some additional test files

--- adaHelper.py
+++ adaHelper.py
@@ -14,8 +14,11 @@ steer_max = 800
 steer_min = 400
 # Gas
 gas_min = 400 #450
+gas_mid = 600 #650 
 gas_max = 800 #850
-
+# Relay
+relay_on = 4095
+relay_off = 0
 
 def map_pulse(val, inMin, inMax, outMin, outMax):
     return int((val - inMin) * (outMax - outMin) / (inMax - inMin) + outMin);
